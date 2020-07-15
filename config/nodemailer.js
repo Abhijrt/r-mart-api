3f4e4334c35f61c3059a7c7aea2f68f90c953aa6
Abhay Jirati <abhi.jrt12@gmail.com>
2020-07-15 14:57:59 +0530

    sending mail by template

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,25 +5,29 @@ const path = require("path");
 
 // importing the ejs
 const ejs = require("ejs");
+const { realpath } = require("fs");
 
+// creating reusable transporter object using the default SMPT transporter
 let transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
-    user: "",
-    pass: "",
+    user: "abhi.jrt12@gmail.com",
+    pass: "abhirlajj",
   },
 });
 
+// rendering template for sending the mail
 let renderTemplate = (data, relativepath) => {
   let mailHTML;
   ejs.renderFile(
-    path.join(__dirname, "./views/mailers/relativepath"),
+    path.join(__dirname, "../views/mailers", relativepath),
+    data,
     function (err, template) {
       if (err) {
-        console.log("Error in rendering the HTML");
+        console.log("Error in rendering the HTML", err);
         return;
       }
       mailHTML = template;
--- mailers/register_mailer.js
+++ mailers/register_mailer.js
@@ -1,15 +1,18 @@
 const nodeMailer = require("../config/nodemailer");
 exports.newRegistration = (user) => {
+  // taking the html content from the template
+  let htmlString = nodeMailer.renderTemplate({ user: user }, "mailer.ejs");
+  // send mail with defined Transporter Object
   nodeMailer.transporter.sendMail(
     {
       from: "abhi.jrt12@gmail.com",
       to: user.email,
       subject: "Registration SuccessFull",
-      html: "<h1>You are registered successfull</h1>",
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
-        console.log("Error in sending the mail");
+        console.log("Error in sending the mail", err);
         return;
       }
       console.log("Message Sent!");
