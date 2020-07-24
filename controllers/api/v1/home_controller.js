4681915c0b47ad52ae45ec52163edfaa88bd7172
Abhay Jirati <abhi.jrt12@gmail.com>
2020-07-24 16:33:00 +0530

    adding the home page

--- controllers/api/v1/home_controller.js
+++ controllers/api/v1/home_controller.js
@@ -1,7 +1,15 @@
+const Products = require("../../../model/stock");
+const Product = require("../../../model/stock");
 // When a home page open this controller will work
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
+  let products = await Product.find({});
+  var newProducts = new Array(products.length);
+  for (let i of products) {
+    newProducts.push(i.toObject());
+  }
   return res.status(200).json({
     message: "It A Home Page",
     success: true,
+    products: newProducts,
   });
 };
