a4d1b02e0cc7c8689e729f30c01fd49519c78853
Abhay Jirati <abhi.jrt12@gmail.com>
2020-07-24 09:03:47 +0530

    adding the product Schema

---
+++ model/stock.js
@@ -0,0 +1,56 @@
+// importing the mongoose
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    stock_quantity: {
+      type: Number,
+      required: true,
+    },
+    marked_price: {
+      type: Number,
+      required: true,
+    },
+    selling_price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    solg_by: {
+      type: String,
+      required: true,
+    },
+    product_image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// to remove a password from an instance
+if (!productSchema.options.toObject) productSchema.options.toObject = {};
+productSchema.options.toObject.transform = function (doc, ret, options) {
+  // delete the passworc and createdAt and UpdatedAt of every document before retuning the result
+  delete ret.createdAt;
+  delete ret.updatedAt;
+  delete ret.__v;
+  delete ret._id;
+  return ret;
+};
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
